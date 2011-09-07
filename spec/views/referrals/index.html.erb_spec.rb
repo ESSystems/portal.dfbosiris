fbe27fb00228824ebaafb806002225f8556c7f6f
Cristian Stănescu <cristianstanescu@gmail.com>
2011-09-07 11:19:53 +0300

    Style referrals list

--- spec/views/referrals/index.html.erb_spec.rb
+++ spec/views/referrals/index.html.erb_spec.rb
@@ -4,4 +4,7 @@ describe "referrals/index.html.erb" do
   
   it "should show patients name"
   
+  it "should show only 200 characters of case nature"
+  
+  it "should show '...' only if case nature is longer than 200 characters"
 end
