e8d4aa92755e66dbb0c0caf38e80dce039bd3c06
Cristian Stănescu <cristianstanescu@gmail.com>
2012-03-07 12:46:05 +0200

    Add production hostname to config.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -5,4 +5,6 @@ when "csmac.local"
   OSIRIS_SERVER_NAME = "cm2.local"
 when "rand.tripledub.co.uk"
   OSIRIS_SERVER_NAME = "cm2.develop.tripledub.co.uk"
+when "IS-16273"
+  OSIRIS_SERVER_NAME = "osiris.clinic-ms.co.uk"
 end
