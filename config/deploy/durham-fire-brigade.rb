e146ab5a5fa766c8da9bcdd20298d6d1653928c7
Cristian Stănescu <cristian@redgrinpot.com>
2013-04-12 15:21:50 +0300

    Change Durham Fire Brigade deployment domain

--- config/deploy/durham-fire-brigade.rb
+++ config/deploy/durham-fire-brigade.rb
@@ -1,5 +1,5 @@
 set :user , "portal"
 server "82.165.137.62", :app, :web, :db , :primary => true
-set :deploy_to , "/var/www/vhosts/portal.clinic-ms.co.uk"
+set :deploy_to , "/var/www/vhosts/portal.dfbosiris.co.uk"
 set :deploy_env, 'production'
 set :rails_env, 'production'
