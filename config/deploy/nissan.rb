6d1bddf6380cf99a0d0c3421600ba5f43069b578
Cristian Stănescu <cristian@redgrinpot.com>
2013-04-18 12:50:58 +0300

    Change Nissan deployemnt server

--- config/deploy/nissan.rb
+++ config/deploy/nissan.rb
@@ -1,5 +1,5 @@
 set :user , "portal"
 server "87.106.136.226", :app, :web, :db , :primary => true
-set :deploy_to , "/var/www/vhosts/osirisnissan.co.uk"
+set :deploy_to , "/var/www/vhosts/portal.osirisnissan.co.uk"
 set :deploy_env, 'production'
 set :rails_env, 'production'
