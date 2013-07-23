506cb0b73181696ed782b63c461b8297cd5fa6a2
Cristian Stănescu <cristian@redgrinpot.com>
2013-07-23 15:45:06 +0300

    Read-only abilities for already created referrals

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,36 +5,44 @@ class Ability
     unless user.nil?
       can :index, :all
 
-      can [:new, :create, :autocomplete_person_full_name, :followers_suggestions], :all do |m|
+      can [:create, :autocomplete_person_full_name, :followers_suggestions], :all do |m|
         !user.read_only_access?
       end
 
-      can [:show, :accept_declination_and_close], Referral do |r|
+      # Referral abilities
+      can :accept_declination_and_close, Referral do |r|
+        !user.read_only_access? && r.try(:referrer) == user
+      end
+
+      can :update, Referral do |r|
+        !user.read_only_access? && r.try(:referrer) == user && !r.try(:canceled?)
+      end
+
+      can :cancel, Referral do |r|
+        !user.read_only_access? && !r.try(:appointments).empty? &&
+          r.try(:referrer) == user && !r.try(:canceled?)
+      end
+
+      can :destroy, Referral do |r|
+        !user.read_only_access? && r.try(:appointments).empty? &&
+          r.try(:referrer) == user && r.try(:state) == 'new'
+      end
+
+      can :show, Referral do |r|
         r.try(:referrer) == user
       end
 
-      can [:edit, :update], Referral do |r|
-        r.try(:referrer) == user && !r.try(:canceled?)
-      end
-
-      can [:cancel], Referral do |r|
-        !r.try(:appointments).empty? && r.try(:referrer) == user && !r.try(:canceled?)
-      end
-
-      can [:destroy], Referral do |r|
-        r.try(:appointments).empty? && r.try(:referrer) == user && r.try(:state) == 'new'
-      end
-
       if user.track_referrals == "all"
-        can [:show], Referral do |r|
+        can :show, Referral do |r|
           r.try(:private) == false || r.try(:private) == true && r.try(:referrer) == user
         end
       elsif user.track_referrals == "initiated_and_assigned"
-        can [:show], Referral do |r|
+        can :show, Referral do |r|
           r.try(:referrer) == user || r.try(:followers).include?(user) && r.try(:private) == false
         end
       end
 
+      # Person abilities
       can [:show], Person
       can [:edit, :update], Person do |p|
         p.try(:referrer) == user
@@ -43,10 +51,13 @@ class Ability
         Appointment.find_by_person_id(p.id).nil? && Referral.find_by_person_id(p.id).nil? && p.try(:referrer) == user
       end
 
+      # User abilities
       can [:dashboard, :edit, :update], User
 
+      # Notification abilities
       can [:index, :read], Notification
 
+      # Appointment abilities
       can [:confirm_appointment, :calendar_data, :calendar_update_date], Appointment do |a|
         a.try(:referral).referrer == user && !a.try(:deleted?)
       end
--- spec/models/abilities/referral/user_with_read_only_access_spec.rb
+++ spec/models/abilities/referral/user_with_read_only_access_spec.rb
@@ -1,28 +1,45 @@
 require 'spec_helper'
 
 describe Ability do
-  let(:referral) { create(:referral) }
+  # let(:another_users_referral) { create(:referral) }
+  # let(:own_referral) { create(:referral, :referrer => user) }
   let(:user) { create(:user, :read_only_access => true) }
 
   before do
     @ability = Ability.new(user)
+    @another_users_referral = create(:referral)
+    @own_referral = create(:referral, :referrer => user)
   end
-
-  it {@ability.should_not be_able_to(:accept_declination_and_close, referral)}
-
-  it {@ability.should_not be_able_to(:cancel, referral)}
 
   it {@ability.should_not be_able_to(:create, Referral.new)}
 
-  it {@ability.should_not be_able_to(:destroy, referral)}
-
   it {@ability.should_not be_able_to(:edit, Referral.new)}
 
-  it {@ability.should be_able_to(:index, referral)}
+  it {@ability.should be_able_to(:index, Referral)}
 
-  it {@ability.should_not be_able_to(:new, referral)}
+  it {@ability.should_not be_able_to(:new, Referral.new)}
 
-  it {@ability.should be_able_to(:show, referral)}
+  shared_examples 'can not make referral changes' do
+    it {@ability.should_not be_able_to(:accept_declination_and_close, referral)}
 
-  it {@ability.should_not be_able_to(:update, referral)}
+    it {@ability.should_not be_able_to(:cancel, referral)}
+
+    it {@ability.should_not be_able_to(:destroy, referral)}
+
+    it {@ability.should be_able_to(:show, referral)}
+
+    it {@ability.should_not be_able_to(:update, referral)}
+  end
+
+  context 'User created the referral' do
+    it_behaves_like 'can not make referral changes' do
+      let(:referral) {@own_referral}
+    end
+  end
+
+  context 'User did not create the referral' do
+    it_behaves_like 'can not make referral changes' do
+      let(:referral) {@another_users_referral}
+    end
+  end
 end
